eb76768bd1870cf816d4c32d92927aa71ca66cf5
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-03 17:19:08 +0100

    feat: add test for empty string parameter
    pass test for empty string parameter

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,4 +1,6 @@
 function capitalize(string) {
+  if (string.trim() === '') return '';
+
   return string[0].toUpperCase() + string.slice(1);
 }
 
--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require('./capitalize');
 test('given a string "alfred" it returns "Alfred"', () => {
     expect(capitalize('alfred')).toBe('Alfred');
 })
+
+test('given an empty string it returns the empty string', () => {
+    expect(capitalize('')).toBe('');
+})
