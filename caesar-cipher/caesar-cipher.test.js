6aa9a995dc4dbe1b321ea039b08b569025cdfb3d
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-02-04 05:47:38 +0100

    feat: add test cases for punctuation and letter case

--- caesar-cipher/caesar-cipher.test.js
+++ caesar-cipher/caesar-cipher.test.js
@@ -2,5 +2,13 @@ const { default: expect } = require('expect');
 const caesarCipher = require('./caesar-cipher');
 
 it('can encrypt a non-empty string', () => {
-  expect(caesarCipher('Hello World!', 1)).toBe('Ifmmp Xpsme!');
+  expect(caesarCipher('Hello World', 1)).toBe('Ifmmp Xpsme');
+});
+
+it('preserve symbols in given string', () => {
+  expect(caesarCipher('!!!', 1)).toBe('!!!');
+});
+
+it('preserve string letter casing', () => {
+  expect(caesarCipher('HELLO world', 1)).toBe('IFMMP xpsme');
 });
