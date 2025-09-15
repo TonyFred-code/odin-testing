0c054ab0cc254467b5b3f1aecfd70fbac061f820
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-15 06:08:53 +0100

    chore: update eslint js config resolution

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,9 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    extends: [js.configs.recommended], // directly use the config object
+    languageOptions: { globals: globals.browser },
+  },
 ]);
