88b0d697b3d9ed7c7951fe9117c049d3252d7d20
Faith A. <111481054+TonyFred-code@users.noreply.github.com>
2025-09-15 05:15:03 +0100

    refactor: update test to use vitest as runner
    import modules using es6modules

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,15 @@
-const sum = require('./sum');
+import { it, expect, describe } from "vitest";
+import sum from "../src/sum.js";
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
-})
+describe("sum", () => {
+  it.each([
+    {
+      firstOperand: 1,
+      secondOperand: 2,
+      result: 3,
+      scenario: "return 3 when given 1 and 2",
+    },
+  ])("should $scenario", ({ firstOperand, secondOperand, result }) => {
+    expect(sum(firstOperand, secondOperand)).toBe(result);
+  });
+});
